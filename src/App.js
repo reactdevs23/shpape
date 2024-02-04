8e5a6c1ead330617b7d01f17bb0eb0fd59274a44
dalimkumar604 <dalimkumar212582@gmail.com>
2024-02-04 10:19:57 +0600

    sadfk

--- src/App.js
+++ src/App.js
@@ -10,28 +10,11 @@ import {
   sql,
   youtube,
 } from "./images";
-import HighlightedLine1 from "./images/SvgComponents/Highlighted/HighlightedLine1";
-import HighlightedLine2 from "./images/SvgComponents/Highlighted/HighlightedLine2";
-import HighlightedLine4 from "./images/SvgComponents/Highlighted/HighlightedLine4";
-import HighlightedLine5 from "./images/SvgComponents/Highlighted/HighlightedLine5";
-import HighlightedLine6 from "./images/SvgComponents/Highlighted/HighlightedLine6";
-import HighlightedLine7 from "./images/SvgComponents/Highlighted/HighlightedLine7";
-import HighlightedLine8 from "./images/SvgComponents/Highlighted/HighlightedLine8";
-import HighlightedLine9 from "./images/SvgComponents/Highlighted/HighlightedLine9";
-import HightlightedLine3 from "./images/SvgComponents/Highlighted/HightlightedLine3";
-import Line from "./images/SvgComponents/Line";
-import Line2 from "./images/SvgComponents/Line2";
-import Line3 from "./images/SvgComponents/Line3";
-import Line4 from "./images/SvgComponents/Line4";
-import Line5 from "./images/SvgComponents/Line5";
-import Line6 from "./images/SvgComponents/Line6";
-import Line7 from "./images/SvgComponents/Line7";
-import Line8 from "./images/SvgComponents/Line8";
-import Line9 from "./images/SvgComponents/Line9";
+
 import Recentgular from "./images/SvgComponents/Recentgular";
 
 function App() {
-  const data = {
+  const allData = {
     fontFamily: "'Inter', sans-serif",
     bgColor: "#000",
     primaryColor: "#fff",
@@ -43,69 +26,80 @@ function App() {
         fourthColor="#5563cf"
       />
     ),
-    first: {
-      icon: pythonLogo,
-      highlight: false,
-      line: <Line color="#6c7079" />,
-      highlightedLine: <HighlightedLine1 color="#d35230" />,
-    },
-    second: {
-      icon: pdfLogo,
-      highlight: false,
-      line: <Line2 color="#6b707b" />,
-      highlightedLine: <HighlightedLine2 color="#c01b24" />,
-    },
-    third: {
-      icon: sql,
-      highlight: true,
-      line: <Line3 color="#6c7079" />,
-      highlightedLine: <HightlightedLine3 color="#db7533" />,
-    },
-    fourth: {
-      icon: nLogo,
-      highlight: true,
-      line: <Line4 color="#69717a" />,
-      highlightedLine: <HighlightedLine4 color="#F1F1F2" />,
-    },
-    fifth: {
-      icon: notepad,
-      highlight: false,
-      line: <Line5 color="#6c7079" />,
-      highlightedLine: <HighlightedLine5 color="#21a366" />,
-    },
-    sixth: {
-      icon: sales,
-      highlight: true,
-      line: <Line6 color="#6b7178" />,
-      highlightedLine: <HighlightedLine6 color="#00A1E0" />,
-    },
-    seventh: {
-      icon: slick,
-      highlight: false,
-      line: <Line7 color="#6b7178" />,
-      highlightedLine: <HighlightedLine7 color="#E01E5A" />,
-    },
-    eighth: {
-      icon: youtube,
-      highlight: false,
-      line: <Line8 color="#6b7178" />,
-      highlightedLine: <HighlightedLine8 color="red" />,
-    },
-    ninth: {
-      icon: file,
-      highlight: false,
-      line: <Line9 color="#6b7178" />,
-      highlightedLine: <HighlightedLine9 color="#508ef5" />,
-    },
+    centerImage: nLogo,
+    data: [
+      {
+        icon: pythonLogo,
+        highlight: false,
+
+        border: "3px solid #6b707b",
+        highlightBorder: "3px dashed #c01b24",
+      },
+      {
+        icon: pdfLogo,
+        highlight: true,
+
+        border: "3px solid #6b707b",
+        highlightBorder: "3px dashed #c01b24",
+      },
+      {
+        icon: sql,
+        highlight: true,
+
+        border: "3px solid #6c7079",
+        highlightBorder: "3px dashed #db7533",
+      },
+      {
+        icon: nLogo,
+        highlight: true,
+
+        border: "3px solid #69717a",
+        highlightBorder: "3px dashed #F1F1F2",
+      },
+      {
+        icon: notepad,
+        highlight: false,
+
+        border: "3px solid #6c7079",
+        highlightBorder: "3px dashed #21a366",
+      },
+      {
+        icon: sales,
+        highlight: true,
+
+        border: "3px solid #6b7178",
+        highlightBorder: "3px dashed #00A1E0",
+      },
+      {
+        icon: slick,
+        highlight: false,
+
+        border: "3px solid #6b7178",
+        highlightBorder: "3px dashed #E01E5A",
+      },
+      {
+        icon: youtube,
+        highlight: false,
+
+        border: "3px solid #6b7178",
+        highlightBorder: "3px dashed #c01b24",
+      },
+      {
+        icon: file,
+        highlight: false,
+        border: "3px solid #6b7178",
+        highlightBorder: "3px dashed #508ef5",
+      },
+    ],
   };
   return (
     <div
       style={{
-        "--primaryColor": data.primaryColor,
-        "--bgColor": data.bgColor,
+        "--primaryColor": allData.primaryColor,
+        "--bgColor": allData.bgColor,
       }}
     >
-      <MainComponent {...data} />
+      <MainComponent {...allData} />
     </div>
   );
 }
--- src/components/MaiComponent/MainComponent.js
+++ src/components/MaiComponent/MainComponent.js
@@ -1,127 +1,58 @@
 import React from "react";
 import styles from "./MainComponent.module.css";
 
-const MainComponent = ({
-  first,
-  second,
-  third,
-  fourth,
-  fifth,
-  sixth,
-  seventh,
-  eighth,
-  ninth,
-  container,
-}) => {
+const MainComponent = ({ data, container, centerImage }) => {
   return (
     <div className={styles.mainWrapper}>
       <section className={styles.wrapper}>
         <div className={styles.mainContainer}>
           <div className={styles.container}>
-            <div className={styles.imagesContainer}>
-              <div
-                className={[
-                  styles.firstImageContainer,
-                  first.highlight && styles.scaleSize,
-                ].join(" ")}
-              >
-                <div className={styles.line1}>
-                  {first.highlight ? first.highlightedLine : first.line}
+            <div
+              className={[
+                styles.imagesContainer,
+                data.length === 3 && styles.only3Items,
+                data.length >= 4 && styles.moreThan3,
+              ].join(" ")}
+            >
+              {data.map((el, i) => (
+                <div
+                  style={{
+                    "--border": el.border,
+                    "--highlightBorder": el.highlightBorder,
+                  }}
+                  className={[
+                    styles.element,
+                    i + 1 === 1 && styles.firstElement,
+                    i + 1 === data.length && styles.lastElement,
+                    (i + 1) % 2 === 1 && i + 1 !== 1 && styles.oddElement,
+                    (i + 1) % 2 === 0 &&
+                      (i + 1) % 4 !== 0 &&
+                      styles.divisibleBy2Not4,
+                    (i + 1) % 2 === 0 &&
+                      (i + 1) % 4 === 0 &&
+                      styles.evenElement,
+                    i + 1 === 2 && styles.secondElement,
+                    i === data.length - 2 && styles.secondLastElement,
+                    el.highlight && styles.highlightElement,
+                  ]
+                    .filter(Boolean)
+                    .join(" ")}
+                >
+                  <div
+                    className={[
+                      styles.border,
+                      el.highlight && styles.highlightBorder,
+                    ].join(" ")}
+                  ></div>
+
+                  <img src={el.icon} alt="" className={styles.icon} />
                 </div>
-                <img src={first.icon} alt="" className={styles.icon} />
-              </div>{" "}
-              <div
-                className={[
-                  styles.secondImageContainer,
-                  second.highlight && styles.scaleSize,
-                ].join(" ")}
-              >
-                <div className={styles.line2}>
-                  {second.highlight ? second.highlightedLine : second.line}
-                </div>
-                <img src={second.icon} alt="" className={styles.icon} />
-              </div>
-              <div
-                className={[
-                  styles.thirdImageContainer,
-                  third.highlight && styles.scaleSize,
-                ].join(" ")}
-              >
-                <div className={styles.line3}>
-                  {third.highlight ? third.highlightedLine : third.line}
-                </div>
-                <img src={third.icon} alt="" className={styles.icon} />
-              </div>{" "}
-              <div
-                className={[
-                  styles.fourthImageContainer,
-                  fourth.highlight && styles.scaleSize,
-                ].join(" ")}
-              >
-                <div className={styles.line4}>
-                  {fourth.highlight ? fourth.highlightedLine : fourth.line}
-                </div>
-                <img src={fourth.icon} alt="" className={styles.icon} />
-              </div>
-              <div
-                className={[
-                  styles.fifthImageContainer,
-                  fifth.highlight && styles.scaleSize,
-                ].join(" ")}
-              >
-                <div className={styles.line5}>
-                  {fifth.highlight ? fifth.highlightedLine : fifth.line}
-                </div>
-                <img src={fifth.icon} alt="" className={styles.icon} />
-              </div>
-              <div
-                className={[
-                  styles.sixthImageContainer,
-                  sixth.highlight && styles.scaleSize,
-                ].join(" ")}
-              >
-                <div className={styles.line6}>
-                  {sixth.highlight ? sixth.highlightedLine : sixth.line}{" "}
-                </div>
-                <img src={sixth.icon} alt="" className={styles.icon} />
-              </div>
-              <div
-                className={[
-                  styles.seventhImageContainer,
-                  seventh.highlight && styles.scaleSize,
-                ].join(" ")}
-              >
-                <div className={styles.line7}>
-                  {seventh.highlight ? seventh.highlightedLine : seventh.line}{" "}
-                </div>
-                <img src={seventh.icon} alt="" className={styles.icon} />
-              </div>{" "}
-              <div
-                className={[
-                  styles.eighthImageContainer,
-                  eighth.highlight && styles.scaleSize,
-                ].join(" ")}
-              >
-                <div className={styles.line8}>
-                  {eighth.highlight ? eighth.highlightedLine : eighth.line}
-                </div>
-                <img src={eighth.icon} alt="" className={styles.icon} />
-              </div>{" "}
-              <div
-                className={[
-                  styles.ninthImageContainer,
-                  ninth.highlight && styles.scaleSize,
-                ].join(" ")}
-              >
-                <div className={styles.line9}>
-                  {ninth.highlight ? ninth.highlightedLine : ninth.line}
-                </div>
-                <img src={ninth.icon} alt="" className={styles.icon} />
-              </div>{" "}
+              ))}
             </div>
 
             <div className={styles.customContainer}>
               <div className={styles.roundedImage}> {container}</div>
+              <img src={centerImage} alt="#" className={styles.centerImage} />
             </div>
           </div>
         </div>
